6c1701b713c6202613809a6320ce2ec6046afc48
Matt Watson <mwatson@renci.org>
2022-12-16 00:35:23 -0500

    change baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://mbwatson.github.io/docs',
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
